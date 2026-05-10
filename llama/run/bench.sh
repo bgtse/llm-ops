e3621eceb08ade82ecbe97846091c7a2be0a9b6d
bgtse <buigiathinh.se@gmail.com>
2026-05-10 11:58:17 +0000

    Support dynamic args for llama bench

--- llama/run/bench.sh
+++ llama/run/bench.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
-source "${SCRIPT_DIR}/../../common/load-env.sh"
 
-"${BIN_DIR}/${BIN_NAME}/llama-bench" \
-  -m "$LLAMA_ARG_MODEL" \
-  -ngl "$LLAMA_ARG_N_GPU_LAYERS" \
-  --flash-attn on \
-  -p 512,2048 \
-  -n 128 \
-  -t 10
+if [ -n "${1:-}" ] && [[ ! "$1" =~ ^- ]]; then
+  source "${SCRIPT_DIR}/../../common/load-env.sh" "$1"
+  shift
+else
+  source "${SCRIPT_DIR}/../../common/load-env.sh" ".env.bench"
+fi
+
+"${BIN_DIR}/${BIN_NAME}/llama-bench" "$@"
